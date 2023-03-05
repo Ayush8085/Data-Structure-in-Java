bff502f571ee7a4404af072ee940831c5b0cae16
Ayush <ayushkumar9601@gmail.com>
2023-03-05 15:35:48 +0530

    Medium

--- Binary Search Problems/Find_in_Rotated_Array.java
+++ Binary Search Problems/Find_in_Rotated_Array.java
@@ -6,55 +6,47 @@ public class Find_in_Rotated_Array {
 
         // int[] arr = { 11, 12, 15, 18, 2, 5, 6, 8 };
         // int[] arr = { 2, 5, 6, 8, 11, 12, 15, 18 };
-        int[] arr = { 2, 5, 6, 8, 11, 12, 15 };
-        int k = 15;
+        // int[] arr = { 2, 5, 6, 8, 11, 12, 15 };
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        // int[] arr = { 4, 5, 6, 7, 0 };
+        int k = 0;
+        int n = arr.length;
 
         int start = 0;
-        int end = arr.length - 1;
+        int end = n - 1;
         int mid = 0;
 
-        while (start <= end) {
-            mid = end - (end - start) / 2;
-
-            if (mid == 0)
-                break;
-            if (arr[mid] < arr[mid + 1] && arr[mid] < arr[mid - 1]) {
-                break;
-            } else if (arr[mid] < arr[mid + 1] && arr[mid] > arr[mid - 1]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-
-        if (mid == 0) {
-            System.out.println(binarySearch(arr, 0, arr.length-1, k));
-        } else {
-
-            if (binarySearch(arr, 0, mid - 1, k) > 0)
-                System.out.println(binarySearch(arr, 0, mid - 1, k));
-            else if (binarySearch(arr, mid + 1, arr.length - 1, k) > 0)
-                System.out.println(binarySearch(arr, mid + 1, arr.length - 1, k));
-        }
-
-    }
-
-    private static int binarySearch(int[] arr, int start, int end, int k) {
-
-        int mid = 0;
-
+        boolean found = false;
         while (start <= end) {
             mid = end - (end - start) / 2;
 
             if (arr[mid] == k) {
-                return mid;
-            } else if (arr[mid] < k) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+                found = true;
+                break;
+            }
+            // check if left is sorted
+            if (arr[mid] > arr[start]) {
+                if (k <= arr[mid] && k >= arr[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // check if right is sorted
+            else {
+                if (k >= arr[mid] && k <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
 
-        return -1;
+        if (found) {
+            System.out.println(mid);
+        } else {
+            System.out.println("Not Found");
+        }
+
     }
 }
